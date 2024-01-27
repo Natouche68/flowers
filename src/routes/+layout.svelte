4d45c71b70e55816c709ca5a2d6f964c2182c4ec
Natouche68 <ngrossheny@gmail.com>
2024-01-27 18:11:27 +0100

    Add the flowers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,10 @@
 </svelte:head>
 
 <slot />
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,24 @@
-<h1>Flowers</h1>
+<script>
+	import Flower from "$lib/Flower.svelte";
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="flowers">
+	>
+	<Flower x={86} />
+	<Flower x={26} y={72} rotation={-20} scale={0.8} />
+	<Flower x={154} y={36} rotation={16} scale={0.84} />
+</svg>
+
+<style>
+	:global(body) {
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #001535;
+	}
+
+	.flowers {
+		height: 80vh;
+	}
+</style>
