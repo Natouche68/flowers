32df77e20e87a0a336f33e9d24ca00993f28025f
Natouche68 <ngrossheny@gmail.com>
2024-01-28 12:53:24 +0100

    Make sure the flowers are always on screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="flowers">
-	>
 	<Flower x={86} />
 	<Flower x={80} y={-30} rotation={-20} scale={0.8} />
 	<Flower x={80} y={20} rotation={16} scale={0.84} />
@@ -20,5 +19,11 @@
 
 	.flowers {
 		height: 80vh;
+	}
+
+	@media screen and (orientation: portrait) {
+		.flowers {
+			width: 80vw;
+		}
 	}
 </style>
